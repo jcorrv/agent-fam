6ce04c11c04e0d38ec4c069c7cfa64de6be9f977
Nathan Moos <moosingin3space@gmail.com>
2025-10-01 19:43:04 -0700

    feat: basic agent loop

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from agent-fam!")
+import asyncio
+import os
+
+from mcp import ClientSession
+from mcp.client.sse import sse_client
+
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+async def main():
+    model = "openai:gpt-5"
+    sse_url = os.environ['ACIDEV_ENDPOINT']
+    async with sse_client(sse_url) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+
+            tools = await load_mcp_tools(session)
+
+            agent = create_react_agent(model, tools)
+
+            # TODO fill in invocation
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
